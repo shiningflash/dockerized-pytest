1144541cbd3060d5748c4307de0fc1611b3bf2f9
Amirul Islam <amirulislamalmamun@gmail.com>
2021-10-09 12:14:43 +0600

    added basic tests and assert

--- tests/chp2/video2/test_mapmaker_start.py
+++ tests/chp2/video2/test_mapmaker_start.py
@@ -1,4 +1,6 @@
+from scripts.chp2.video2.mapmaker_start import Point
 
 
 def test_make_one_point():
-    pass
+    p1 = Point("Dhaka", 13.3456, 17.2314)
+    assert p1.get_lat_long() == (13.3456, 17.2314)
